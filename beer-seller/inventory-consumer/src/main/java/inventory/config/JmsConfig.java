73d1fc588e2d5359a39b4d3edffddad1e4e1dc7e
bruno.monteiro <bruno.monteiro@ab-inbev.com>
2023-08-18 22:38:50 -0300

    basic mvn clean install

--- beer-seller/inventory-consumer/src/main/java/inventory/config/JmsConfig.java
+++ beer-seller/inventory-consumer/src/main/java/inventory/config/JmsConfig.java
@@ -1,37 +1,37 @@
-//package inventory.config;
-//
-//import jakarta.jms.ConnectionFactory;
-//import org.apache.activemq.ActiveMQConnectionFactory;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-//import org.springframework.jms.config.JmsListenerContainerFactory;
-//
-//@Configuration
-//public class JmsConfig {
-//    private static final String BROKER_URL = "tcp://localhost:61616";
-//
-//    @Bean
-//    public ConnectionFactory connectionFactory() {
-//        final ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-//        connectionFactory.setBrokerURL(BROKER_URL);
-//        return connectionFactory;
-//    }
-//
-//    @Bean
-//    public JmsListenerContainerFactory<?> queueListenerFactory(final ConnectionFactory connectionFactory) {
-//        final DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-//        factory.setConnectionFactory(connectionFactory);
-//        factory.setPubSubDomain(false); // Set to false for queues
-//        return factory;
-//    }
-//
-//    @Bean
-//    public JmsListenerContainerFactory<?> topicListenerFactory(final ConnectionFactory connectionFactory) {
-//        final DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-//        factory.setConnectionFactory(connectionFactory);
-//        factory.setPubSubDomain(true); // Set to true for topics
-//        return factory;
-//    }
-//}
-//
+package inventory.config;
+
+import jakarta.jms.ConnectionFactory;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+
+@Configuration
+public class JmsConfig {
+    private static final String BROKER_URL = "tcp://localhost:61616";
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        final ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL(BROKER_URL);
+        return connectionFactory;
+    }
+
+    @Bean
+    public JmsListenerContainerFactory<?> queueListenerFactory(final ConnectionFactory connectionFactory) {
+        final DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setPubSubDomain(false); // Set to false for queues
+        return factory;
+    }
+
+    @Bean
+    public JmsListenerContainerFactory<?> topicListenerFactory(final ConnectionFactory connectionFactory) {
+        final DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setPubSubDomain(true); // Set to true for topics
+        return factory;
+    }
+}
+
